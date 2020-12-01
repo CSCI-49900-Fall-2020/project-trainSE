554a5281fd527c0d95acdc9cac201e4feba17862
Kareem Hussein <kareemhussein41@gmail.com>
2020-12-01 16:53:09 -0500

    Testing deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express"); // Including the express module in this file
 const mongoose = require("mongoose"); // For MongoDB
 const cors = require("cors");
+const path = require("path"); // For deployment
 
 // Set up express
 const app = express();
@@ -27,6 +28,20 @@ app.use("/api/users", require("./routes/api/userRouter"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/domain", require("./routes/api/domainRouter"));
 app.use("/api/upload", require("./routes/api/uploadRouter"));
+
+// Serve static assests (the build folder) if we're in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // Any request that we get that isn't the routes middleware
+  // These requests must be wanting to fetch front end react pages instead of accessing backend api routes
+  app.get("*", (req, res) => {
+    return res.sendFile(
+      path.resolve(__dirname, "client", "build", "index.html")
+    );
+  });
+}
 
 // Port configuration
 const PORT = process.env.PORT || 5000;
