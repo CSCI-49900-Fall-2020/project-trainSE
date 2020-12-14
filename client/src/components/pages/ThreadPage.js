16d62109685e4df3dee51827ad60d17a388e02e2
Kareem Hussein <kareemhussein41@gmail.com>
2020-12-14 15:38:38 -0500

    Add dropdown to sort by most liked, still neeed help with organizing by timestamp

--- client/src/components/pages/ThreadPage.js
+++ client/src/components/pages/ThreadPage.js
@@ -8,6 +8,7 @@ import {
   Segment,
   Icon,
   Button,
+  Dropdown,
 } from "semantic-ui-react";
 import { useRouteMatch, Link } from "react-router-dom";
 import Axios from "axios";
@@ -19,6 +20,29 @@ import ResourceItem from "../layout/ResourceItem";
 
 // The ThreadPage component as a functional component
 const ThreadPage = ({ auth: { user } }) => {
+  // -------------------- STATIC DATA --------------------------------------------------
+
+  const sortingOptions = [
+    {
+      key: "most liked",
+      text: "Most Liked",
+      value: "Most Liked",
+    },
+    {
+      key: "recent to oldest",
+      text: "Recent to Oldest",
+      value: "Recent to Oldest",
+    },
+    {
+      key: "oldest to recent",
+      text: "Oldest to Recent",
+      value: "Oldest to Recent",
+    },
+  ];
+  // -------------------- STATIC DATA --------------------------------------------------
+
+  // -------------------- LOCAL REACT STATE --------------------------------------------------
+
   const [resources, setResources] = useState([]);
   const [difficulty, setDifficulty] = useState("");
   const [threadTitle, setThreadTitle] = useState("");
@@ -31,7 +55,9 @@ const ThreadPage = ({ auth: { user } }) => {
   let discipline = routing_params[2];
   let repository = routing_params[3];
   let thread = routing_params[5];
+  // -------------------- LOCAL REACT STATE --------------------------------------------------
 
+  // -------------------- MISCELLANEOUS --------------------------------------------------
   // Helper function for formatting a thread title when there are no resources present
   const formatTitle = (emptyThread) => {
     emptyThread = emptyThread.replace(/-/g, " ");
@@ -49,6 +75,13 @@ const ThreadPage = ({ auth: { user } }) => {
     return emptyThread;
   };
 
+  // Helper function to map resource info to <ResourceItem/> component
+  const resourceList = resources.map((resource) => (
+    <ResourceItem resource={resource} />
+  ));
+  // -------------------- MISCELLANEOUS --------------------------------------------------
+
+  // -------------------- USE EFFECT API CALLS --------------------------------------------------
   // Making side effect Axios call to retrieve the resources belonging to the thread specified in the url
   useEffect(() => {
     async function fetchThreads() {
@@ -80,12 +113,9 @@ const ThreadPage = ({ auth: { user } }) => {
     // Call the asynchronous function
     fetchThreads();
   }, []);
+  // -------------------- USE EFFECT API CALLS --------------------------------------------------
 
-  // Helper function to map resource info to <ResourceItem/> component
-  const resourceList = resources.map((resource) => (
-    <ResourceItem resource={resource} />
-  ));
-
+  // -------------------- CONDITIONALLY RENDERED HELPER FUNCTIONS --------------------------------------------------
   // Helper function to conditionally render difficulty colors
   let determineColor = (difficulty) => {
     if (difficulty === "Beginner") return "green";
@@ -115,36 +145,90 @@ const ThreadPage = ({ auth: { user } }) => {
       </Link>
     </Segment>
   );
+  // -------------------- CONDITIONALLY RENDERED HELPER FUNCTIONS --------------------------------------------------
+
+  // If a choice is made from the dropdown to sort
+  const onChangeDropdown = (e, result) => {
+    console.log("This is the triggered event: ", e);
+    console.log("This is the actual values: ", result);
+    const { value } = result || e.target;
+    console.log(value);
+
+    // Important article:
+    //https://dev.to/ramonak/react-how-to-dynamically-sort-an-array-of-objects-using-the-dropdown-with-react-hooks-195p
+    // Sorting while updating the UI requires use of the spread operator
+
+    // If a user clicked on to sort by most likes
+    if (value === "Most Liked") {
+      // Sort by ascending to descending likes
+      const organizedByMostLiked = [...resources].sort(
+        (resource1, resource2) => {
+          return resource2.likes - resource1.likes;
+        }
+      );
+      console.log(organizedByMostLiked);
+      setResources(organizedByMostLiked);
+    }
+    // If a user clicked on to sort by most recent to oldest
+    else if (value === "Recent to Oldest") {
+      const organizedByMostRecent = [...resources].sort(
+        (resource1, resource2) => {
+          // return resource1.timestamp - resource2.timestamp;
+        }
+      );
+      // console.log(organizedByMostLiked);
+      // setResources(organizedByMostRecent);
+    }
+    // If a user clicked on to sort by oldest to recent
+    else if (value === "Oldest to Recent") {
+      const organizedByOldest = [...resources].sort((resource1, resource2) => {
+        // console.log(resource2.timestamp - resource1.timestamp);
+      });
+      // console.log(organizedByMostLiked);
+      // setResources(organizedByOldest);
+    }
+
+    // Update the state of the resource based on the user's choice
+    // setResource({ ...resource, [name]: value });
+  };
 
   // The actual HTML/JSX to return after a component is mounted
   return (
     <React.Fragment>
       {/* {console.log("Resources after api call: ", resources)} */}
-        {/* Main Section */}
-          <Container >
-            {/* The main header thread title */}
-            <Grid style={{ backgroundColor: "teal", padding: "45px 0 25px 0" }}>
-            <span>
-              <Header as="h1" textAlign="center" inverted>
-                {threadTitle} Thread
-              </Header>
-            </span>
-            </Grid>
-            <Grid style={{ padding: "1% 0 2% 1%" }}>
-            {/* Specify the repository this thread belongs to */}
-            <Label color="blue" size="large">
-              {repositoryTitle}
-            </Label>{" "}
-            {/* Specify the difficulty level of this thread */}
-            <Label color={determineColor(difficulty)} size="large">
-              {difficulty}
-            </Label>
-            </Grid>
-          </Container>
+      {/* Main Section */}
+      <Container>
+        {/* The main header thread title */}
+        <Grid style={{ backgroundColor: "teal", padding: "45px 0 25px 0" }}>
+          <span>
+            <Header as="h1" textAlign="center" inverted>
+              {threadTitle} Thread
+            </Header>
+          </span>
+        </Grid>
+        <Grid style={{ padding: "1% 0 2% 1%" }}>
+          {/* Specify the repository this thread belongs to */}
+          <Label color="blue" size="large">
+            {repositoryTitle}
+          </Label>{" "}
+          {/* Specify the difficulty level of this thread */}
+          <Label color={determineColor(difficulty)} size="large">
+            {difficulty}
+          </Label>
+          {/* Dropdown to sort */}
+          <Dropdown
+            placeholder="Sort By"
+            selection
+            clearable
+            options={sortingOptions}
+            onChange={onChangeDropdown}
+          />
+        </Grid>
+      </Container>
 
-          {/* Rendering the actual resources */}
-          {/* Conditionally rendering if the reso */}
-          {emptyResources ? loading : populateResources}
+      {/* Rendering the actual resources */}
+      {/* Conditionally rendering if the repo are empty or they exist */}
+      {emptyResources ? loading : populateResources}
     </React.Fragment>
   );
 };
